9685b94c6bcaa1360dd6359550daf9f3b5430b02
60iTamas <80523010+60iTomi@users.noreply.github.com>
2024-08-23 20:13:19 +0200

    chore: Refactor menu animation and styling

--- main.js
+++ main.js
@@ -1,9 +1,7 @@
 document.getElementById("menuOpen").addEventListener("click", function() {
     document.getElementById("menu").classList.add("menuOpened");
-    document.getElementById("menu").classList.remove("menuClosed");
 });
 
 document.getElementById("menuExit").addEventListener("click", function() {
     document.getElementById("menu").classList.remove("menuOpened");
-    document.getElementById("menu").classList.add("menuClosed");
 });
