190dff74e535dac2e5e25f936ec0a6880d157c75
60iTamas <80523010+60iTomi@users.noreply.github.com>
2024-08-24 08:40:33 +0200

    chore: Update menu styling and behavior to match design guidelines

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 document.getElementById("menuOpen").addEventListener("click", function() {
+    document.getElementById("menu").classList.remove("menuClosed");
     document.getElementById("menu").classList.add("menuOpened");
+    
 });
 
 document.getElementById("menuExit").addEventListener("click", function() {
     document.getElementById("menu").classList.remove("menuOpened");
+    document.getElementById("menu").classList.add("menuClosed");
 });
