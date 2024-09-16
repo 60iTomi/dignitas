1a8040191d2e1dfcdf3e633dbc852f1b05acc13e
60iTomi <hatvani.tamas.07@gmail.com>
2024-09-16 17:15:15 +0200

    style: Update map CSS and JavaScript for improved design

--- map.js
+++ map.js
@@ -4,7 +4,7 @@ var map = L.map('map').setView([47.4932055, 19.0527219], 16);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a><p>Love, DIGNITAS</p>'
 }).addTo(map);
 
 
