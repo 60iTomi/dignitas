fe3c46b392dcfffc4d16071b57c5a16cb63848d6
60iTomi <hatvani.tamas.07@gmail.com>
2024-09-18 19:27:23 +0200

    Fix menu overflow issue

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 document.getElementById("menuOpen").addEventListener("click", function() {
     document.getElementById("menu").classList.remove("menuClosed");
     document.getElementById("menu").classList.add("menuOpened");
-    
+    document.documentElement.style.setProperty('overflow-y', 'hidden');
 });
 
 document.getElementById("menuExit").addEventListener("click", function() {
     document.getElementById("menu").classList.remove("menuOpened");
     document.getElementById("menu").classList.add("menuClosed");
+    document.documentElement.style.setProperty('overflow-y', 'auto');
 });
