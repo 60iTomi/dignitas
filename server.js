c5906f50693f25ecc32c5f8fa47f58486b323b6a
60iTamas <80523010+60iTomi@users.noreply.github.com>
2024-11-24 12:33:24 +0000

    update environment variable for API key in server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const __dirname = path.dirname(__filename);
 // Serve static files from 'public' directory
 app.use(express.static(path.join(__dirname, 'public')));
 
-const key = process.env.thomasDev;
+const key = process.env.localKey;
 const databaseID = "673c4f1b00129f34b96a"
 const establishmentsID = "673c5142003d61bb913a";
 const codesID = "673c4f49003246467a94";
